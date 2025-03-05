25dc44dced8b6d8232fd2ca75e09a1605bb3b5c6
sachenpather <141023487+sachenpather@users.noreply.github.com>
2025-03-05 12:55:05 +0200

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -56,6 +56,8 @@ app.UseStaticFiles();
 app.UseRouting();
 app.UseAuthorization();
 
+app.MapControllers();
+
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
